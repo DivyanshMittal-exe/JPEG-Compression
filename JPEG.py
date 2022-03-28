db6bc3b1cb64b5a05e8eaf5320f01d5580bc8127
Divyansh Mittal <78041366+DivyanshMittal-exe@users.noreply.github.com>
2022-03-29 00:42:29 +0530

    Compressed to 5% Uncompression left

--- JPEG.py
+++ JPEG.py
@@ -1,9 +1,10 @@
 import cv2
+import os
 import  numpy as np
 from Subsample import *
 from DFT import *
 from Quantisation import *
-from HuffmanEncode import *
+# from HuffmanEncode import *
 
 class jpeg:
     Chroma_Scale = 2
@@ -11,19 +12,48 @@ class jpeg:
     def __init__(self):
         pass
     
-    def write_orignal(img,name,header):
+    def write_orignal(self,img,name,header):
         row,col = img.shape
         np.savetxt(name + ".txt", img, fmt="%d", header=header) 
+        return os.path.getsize(name + ".txt")
+    
+    def write_compress(self,data_list,name,header):
+        f = open(name + ".txt", "w")
+        f.write(header)
+        for dat in data_list:
+            npstr = np.array2string(dat.astype(np.int8),max_line_width=10000)[1:-1]
+            npstr = " ".join(npstr.split())
+            # print(npstr)
+            f.write(npstr)
+            f.write("\n")
+        f.close()
+        return os.path.getsize(name + ".txt")
+        
+    
+    def compress_channel(self,img,name,header):
+        row,col = img.shape
+        data_list = []
+        # data_arr = np.empty(shape=(0, 0))
+        r,c = row//8,col//8
+        for i in range(r):
+            for j in range(c):
+                block = img[i*8:(i+1)*8,j*8:(j+1)*8]
+                block = dft(block)
+                block = Quant_Y(block)
+                arr = zigzagflat(block)
+                arr = np.trim_zeros(arr, 'b')
+                data_list.append(arr)
+        Yheader = str(r) + " " + str(c) + " " + header + "\n"
+        return self.write_compress(data_list,name+header,Yheader)
         
     
     def compress(self,path,un_compress_name,compress_name):
-        
         img = cv2.imread(path)
         row,col,c = img.shape
         
-        self.write_orignal(img[:,:,0],un_compress_name + "R","R")
-        self.write_orignal(img[:,:,1],un_compress_name + "G","G")
-        self.write_orignal(img[:,:,2],un_compress_name + "B","B")
+        r = self.write_orignal(img[:,:,0],un_compress_name + "R","R")
+        g = self.write_orignal(img[:,:,1],un_compress_name + "G","G")
+        b = self.write_orignal(img[:,:,2],un_compress_name + "B","B")
         
         img = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
         
@@ -35,33 +65,36 @@ class jpeg:
         Cb = avg_subsample(Cb,self.Chroma_Scale)
         Cr = avg_subsample(Cr,self.Chroma_Scale)
          
-        data_list = []
-        # data_arr = np.empty(shape=(0, 0))
-        r,c = row//8,col//8
-        for i in range(r):
-            for j in range(c):
-                block = Y[i*8:(i+1)*8,j*8:(j+1)*8]
-                block = dft(block)
-                block = Quant_Y(block)
-                arr = zigzagflat(block)
-                arr = np.trim_zeros(arr, 'b')
-                data_list.append(arr)
-                
-        self.write_compress()
-        # data_arr = np.append(data_arr,arr)
-        # print(data_arr.shape)
-        # codecY,hof_dataY = hof(data_arr)
-        # print(len(codecY.decode(hof_dataY)))
-        # print(codecY)
-        # print(len(hof_dataY))
-                
+        comp_y = self.compress_channel(Y,compress_name,"Y")
+        comp_cb = self.compress_channel(Cb,compress_name,"Cb")
+        comp_cr = self.compress_channel(Cr,compress_name,"Cr")
         
-        cv2.imshow('Raw',Y)
-        cv2.waitKey(0)
+        print("Original image size: " + str(r+g+b))
+        print("Compressed image size: " + str(comp_y+comp_cb+comp_cr))
+        
+        print("Compressed size compared to original: " + str(round((comp_y+comp_cb+comp_cr)*100/(r+g+b),3)) + "%")
+    
+    
+    def lines_to_matrix(self,data_list):
         pass
+    
+    def uncompress(self,y,cb,cr):
+        f = open(y + ".txt", "r")
+        y = f.readlines()
+        f.close()
+        
+        f = open(cb + ".txt", "r")
+        cb = f.readlines()
+        f.close()
+        
+        f = open(cr + ".txt", "r")
+        cr = f.readlines()
+        f.close()
+        
+        self.lines_to_matrix(y)
 
 if __name__ == '__main__':
     a = jpeg()
-    path = "mountain.jpg"
+    path = "flower.jpg"
     # path = "dog.NEF"
-    a.compress(path)
+    a.compress(path,"original","compressed")
